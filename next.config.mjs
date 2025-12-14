1e680e75d4924b4b4c6ae2eafb3aa8d72fa5e0b5
Fastreaa <foboseaa@gmail.com>
2025-12-14 16:58:50 -0600

    s

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,10 @@
-import nextMDX from '@next/mdx';
-
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Esta línea es la que permite que Next.js lea archivos .md como páginas
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
-// Exportamos la configuración envuelta en withMDX
-export default withMDX(nextConfig);
+export default nextConfig;
