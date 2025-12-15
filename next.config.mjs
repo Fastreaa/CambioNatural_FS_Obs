1fb22e35a66a822e4d6a1ec2b9c05d0c5a475f3a
Fastreaa <foboseaa@gmail.com>
2025-12-14 18:24:34 -0600

    move

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,12 @@
-import createMDX from "@next/mdx";
+import nextMDX from '@next/mdx'
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  trailingSlash: true,
+const withMDX = nextMDX({
+  extension: /\.mdx?$/
+})
+
+export default withMDX({
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   images: {
-    unoptimized: true,
-  },
-  pageExtensions: ["js", "jsx", "mdx"],
-};
-
-const withMDX = createMDX({
-  extension: /\.mdx$/,
-});
-
-export default withMDX(nextConfig);
+    unoptimized: false
+  }
+})
